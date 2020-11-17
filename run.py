4c5af75d4a5b221bde94631d7b19263f2b7c0e4f
lepszytomek <lepszytomek@gmail.com>
2020-11-17 16:07:54 +0100

    jest

--- run.py
+++ run.py
@@ -1,1 +1,34 @@
-import
+import pygame, sys
+
+
+
+class Game(object):
+
+
+    def __init__(self):
+        pygame.init()
+        self.screen_x = 1500
+        self.screen_y = 1000
+        self.screen = pygame.display.set_mode((self.screen_x, self.screen_y))
+
+
+
+        while True:
+            # manewrowanie oknem
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    sys.exit(0)
+                elif event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE:
+                    sys.exit(0)
+
+
+
+
+
+
+
+
+
+if __name__ == "__main__":
+    Game()
+

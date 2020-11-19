8b38af6ec82e08e69f29814684d44afd92bef8d4
kazioktroll <woj.kwi03@wp.pl>
2020-11-19 15:10:00 +0100

    append Card class

--- Card.py
+++ Card.py
@@ -1,18 +1,19 @@
 import pygame
-from run import game
 
 
 class Card(object):
 
 
-    def __init__(self, size, position, type):
-        #type (value *1-13*, color *0-3*), ex. (3, 2)
-        #colors 0:spades(pik) 1:hearts(kier) 2:diamonds(karo) 3:clubs(trefl)
-        self.game = game
+    def __init__(self, Game, size, position, type):
+        #type (value *1-13*, color *1-4*), ex. (3, 2)
+        #colors 1:spades(pik) 2:hearts(kier) 3:diamonds(karo) 4:clubs(trefl)
+        self.Game = Game
         self.size = size
         self.position = position
         self.type = type
         self.rect = pygame.Rect(self.position, self.size)   #Potencjalnie można tu dodać zaokrąglanie rogów
+        self.open = False
 
     def draw(self):
+
         pygame.draw.rect(self.game.screen, (200, 50, 50), self.rect)
